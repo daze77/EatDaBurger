c7cfab99cd0dba0f1a8324aabcb988460c557c7c
daze_77 <daze77@gmail.com>
2021-03-11 19:06:08 -0500

    added JAWSDB code to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,36 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
+
+// use this wrapper to create promise around mysql
+class Database {
+  constructor( config ) {
+      this.connection = mysql.createConnection( 
+          process.env.JAWSDB_URL ? process.env.JAWSDB_URL : config );
+  }
+  query( sql, args=[] ) {
+      return new Promise( ( resolve, reject ) => {
+          this.connection.query( sql, args, ( err, rows ) => {
+              if ( err )
+                  return reject( err );
+              resolve( rows );
+          } );
+      } );
+  }
+  close() {
+      return new Promise( ( resolve, reject ) => {
+          this.connection.end( err => {
+              if ( err )
+                  return reject( err );
+              resolve();
+          } );
+      } );
+  }
+}
+
+
+
+
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
