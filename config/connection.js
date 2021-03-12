6582dcd53c9e7786c729460a688c247911e5e3a1
daze_77 <daze77@gmail.com>
2021-03-11 19:41:36 -0500

    made changes to try and get connection to work

--- config/connection.js
+++ config/connection.js
@@ -40,14 +40,14 @@ const connection = mysql.createConnection({
   database: 'burgers_db',
 });
 
-// Make connection.
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
-});
+// // Make connection.
+// connection.connect((err) => {
+//   if (err) {
+//     console.error(`error connecting: ${err.stack}`);
+//     return;
+//   }
+//   console.log(`connected as id ${connection.threadId}`);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
