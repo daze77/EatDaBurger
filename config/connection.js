c23eca4040803cad3bcc10bc9cf6057740c0da8e
daze_77 <daze77@gmail.com>
2021-03-12 13:49:49 -0500

    updates to heroku connection

--- config/connection.js
+++ config/connection.js
@@ -43,15 +43,15 @@ class Database {
 //     })
 //     return db
 // }
-// const connection=mysql.createConnection(process.env.JAWSDB_URL)
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'Summer77',
-  database: 'burgers_db',
-});
+const connection=mysql.createConnection(process.env.JAWSDB_URL)
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//   // NOTE: Be sure to add your MySQL password here!
+//   password: 'Summer77',
+//   database: 'burgers_db',
+// });
 
 // // Make connection.
 // connection.connect((err) => {
