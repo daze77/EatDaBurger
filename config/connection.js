6fa1cb76d73efa39f50df77a2a0b455e49d76e2d
daze_77 <daze77@gmail.com>
2022-09-22 14:49:15 -0400

    adjustements to connection file

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const mysql = require('mysql2');
 class Database {
   constructor( config ) {
       this.connection = mysql.createConnection( 
-          process.env.ROACHDB_URL ? process.env.ROACHDB_URL : config );
+          process.env.DATABASE_URL ? process.env.DATABASE_URL : config );
   }
   query( sql, args=[] ) {
       return new Promise( ( resolve, reject ) => {
